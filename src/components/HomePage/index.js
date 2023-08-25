2d849da66f407bb6c751b34bc55c5e239872c6a8
sailokesh <sailokeshchirumarri@gmail.com>
2023-08-25 03:37:21 +0000

    2nd commit

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -5,47 +5,9 @@ import Header from '../Header'
 import Footer from '../Footer'
 import './index.css'
 
-const companyLogosData = [
-  {
-    id: 'd14af630-5d22-4bfb-85dd-bb507b139b82',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475819/Company%20Logos%20/netflix-img_keqbbh.png',
-  },
-  {
-    id: '0a932287-8002-4fc8-95d1-8cbed3224e37',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/facebook-img_fxqbxv.png',
-  },
-  {
-    id: '8211ce0c-d7dc-4d2b-8468-5e48ad9ae985',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/google-img_cnnaol.png',
-  },
-  {
-    id: 'daa48153-3f16-4797-8469-5d63c9cba938',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/zomato-img_gb1k9w.png',
-  },
-  {
-    id: '2f4b518e-29b3-45c3-a7a5-80929f7898d9',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/swiggy-img_jgxg6g.png',
-  },
-  {
-    id: '895b9b4d-a283-4ee1-9fb8-933a3c4b449c',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/amazon-img_yvggab.png',
-  },
-  {
-    id: 'a8c67fd0-bab9-46ec-95de-cbfa2e3473f6',
-    companyLogoUrl:
-      'https://res.cloudinary.com/do4qwwms8/image/upload/v1639475818/Company%20Logos%20/flipkart-img_cb70ic.png',
-  },
-]
-
 const settings = {
   dots: false,
-  infinite: true,
+  infinite: false,
   speed: 500,
   slidesToShow: 4,
   slidesToScroll: 1,
@@ -74,9 +36,32 @@ const settings = {
   ],
 }
 
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  loading: 'LOADING',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+
 class HomePage extends Component {
+  state = {trendingMovies: [], apiStatus: apiStatusConstants.initial}
+
   componentDidMount() {
     this.getTrendingMovies()
+  }
+
+  updateTrendingMovies = trendingMovies => {
+    const updateDetails = trendingMovies.results.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      posterPath: eachItem.poster_path,
+      backdropPath: eachItem.backdrop_path,
+    }))
+    this.setState({
+      trendingMovies: updateDetails,
+      apiStatus: apiStatusConstants.success,
+    })
+    console.log(updateDetails)
   }
 
   getTrendingMovies = async () => {
@@ -92,25 +77,29 @@ class HomePage extends Component {
     if (response.ok) {
       const trendingMovies = await response.json()
       console.log(trendingMovies)
+      this.updateTrendingMovies(trendingMovies)
     }
   }
 
-  renderSlider = () => (
-    <Slider {...settings}>
-      {companyLogosData.map(eachLogo => {
-        const {id, companyLogoUrl} = eachLogo
-        return (
-          <div className="slick-item" key={id}>
-            <img
-              className="logo-image"
-              src={companyLogoUrl}
-              alt="company logo"
-            />
-          </div>
-        )
-      })}
-    </Slider>
-  )
+  renderSlider = () => {
+    const {trendingMovies} = this.state
+    return (
+      <Slider {...settings}>
+        {trendingMovies.map(eachLogo => {
+          const {id, backdropPath} = eachLogo
+          return (
+            <div className="slick-item" key={id}>
+              <img
+                className="logo-image"
+                src={backdropPath}
+                alt="company logo"
+              />
+            </div>
+          )
+        })}
+      </Slider>
+    )
+  }
 
   render() {
     return (
